a146605892ebd0624fc74a4d861d650ea61c687a
EC2 Default User <ec2-user@ip-172-31-32-18.ec2.internal>
2021-04-08 05:07:46 +0000

    [Finish]3-5

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -4,15 +4,30 @@ class TodolistsController < ApplicationController
     @list = List.new
   end
 
+    # 以下を追加
   def create
+    # １. データを新規登録するためのインスタンス作成
     list = List.new(list_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    redirect_to '/top'
+    # ３. トップ画面へリダイレクト
+    # redirect_to '/top' を削除して、以下コードに変更
+    # 詳細画面へリダイレクト
+    redirect_to todolist_path(list.id)
   end
 
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+
   private
+  # ストロングパラメータ
   def list_params
     params.require(:list).permit(:title, :body)
   end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_28_050512) do
+ActiveRecord::Schema.define(version: 2021_03_25_061057) do
 
   create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_id"
   end
 
 end
